713b90ee705cb42a7410e0169dc46add211fe206
mauromltn <mauromtne@icloud.com>
2025-04-28 18:14:57 +0200

    feat: Update Hero, add icon.
    - Update hero component with text and quiz button
    - Add a custom icon

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <main className='flex flex-col items-center mt-20'>
+      <h1 className='w-180 text-4xl font-bold text-center mb-8'>
+        FAI IL NOSTRO QUIZ PER SCOPRIRE IL TUO <span className='text-neutral-500'>MOOD</span> E TROVARE LA MIGLIOR ESPERIENZA DI <span className='text-neutral-500'>VISIONE</span>
+      </h1>
+      <button className='bg-orange-600 py-4 px-6 rounded-full font-bold hover:bg-orange-700 hover:text-white/80 duration-200'>
+        FAI IL QUIZ
+      </button>
+    </main>
   )
 }
